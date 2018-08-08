983171e9702452f87ddc468db8cd9a6151eb7406
HenryHK <henryhan.hku@gmail.com>
2018-08-09 09:42:12 +1000

    add splash to android; update android min api to 21

--- android/app/src/main/java/com/example/demo/MainActivity.java
+++ android/app/src/main/java/com/example/demo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -8,6 +11,18 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    // make transparent status bar
+    getWindow().setStatusBarColor(0x00000000);
+    // this line is auto generated
     GeneratedPluginRegistrant.registerWith(this);
+    //Remove full screen flag after load
+    ViewTreeObserver vto = getFlutterView().getViewTreeObserver();
+    vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+      @Override
+      public void onGlobalLayout() {
+        getFlutterView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      }
+    });
   }
 }
